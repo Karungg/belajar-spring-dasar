c36595ba5ae9bc64648f7d8ba4d347737c103794
Karungg <miftahfadilah71@gmail.com>
2025-05-27 18:58:50 +0700

    Custom Scope

--- src/main/java/belajarspringdasar/belajar_spring_dasar/ScopeConfiguration.java
+++ src/main/java/belajarspringdasar/belajar_spring_dasar/ScopeConfiguration.java
@@ -1,10 +1,12 @@
 package belajarspringdasar.belajar_spring_dasar;
 
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import belajarspringdasar.belajar_spring_dasar.data.Foo;
+import belajarspringdasar.belajar_spring_dasar.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,6 +17,20 @@ public class ScopeConfiguration {
     @Scope("prototype")
     public Foo foo() {
         log.info("FooScope bean created");
+        return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean(value = "fooDoubleton")
+    @Scope("doubleton")
+    public Foo fooDooubleton() {
+        log.info("Create new foo");
         return new Foo();
     }
 
--- src/test/java/belajarspringdasar/belajar_spring_dasar/ScopeConfigurationTest.java
+++ src/test/java/belajarspringdasar/belajar_spring_dasar/ScopeConfigurationTest.java
@@ -1,6 +1,7 @@
 package belajarspringdasar.belajar_spring_dasar;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -10,13 +11,32 @@ import belajarspringdasar.belajar_spring_dasar.data.Foo;
 
 @SpringBootTest
 public class ScopeConfigurationTest {
+
+    private ApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        context = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+    }
+
     @Test
     void testScope() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
-
         Foo foo = context.getBean("fooScope", Foo.class);
         Foo foo1 = context.getBean("fooScope", Foo.class);
 
         Assertions.assertNotSame(foo, foo1);
+    }
+
+    @Test
+    void testCustomScope() {
+        Foo foo1 = context.getBean("fooDoubleton", Foo.class);
+        Foo foo2 = context.getBean("fooDoubleton", Foo.class);
+        Foo foo3 = context.getBean("fooDoubleton", Foo.class);
+        Foo foo4 = context.getBean("fooDoubleton", Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo3, foo4);
+        Assertions.assertSame(foo1, foo3);
+        Assertions.assertSame(foo2, foo4);
     }
 }
