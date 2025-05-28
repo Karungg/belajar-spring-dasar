217abf2f8a561f7e395eb6c9617efafdc6662f38
Karungg <miftahfadilah71@gmail.com>
2025-05-28 10:08:11 +0700

    Life cycle annotation

--- src/test/java/belajarspringdasar/belajar_spring_dasar/LifeCycleTest.java
+++ src/test/java/belajarspringdasar/belajar_spring_dasar/LifeCycleTest.java
@@ -8,21 +8,28 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import belajarspringdasar.belajar_spring_dasar.data.Connection;
+import belajarspringdasar.belajar_spring_dasar.data.Server;
 
 @SpringBootTest
 public class LifeCycleTest {
-    private ConfigurableApplicationContext context;
-
-    @BeforeEach
-    void setUp() {
-        context = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
-    }
-
     @Test
     void testLifeCycle() {
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
         Connection connection = context.getBean(Connection.class);
 
         Assertions.assertNotNull(connection);
+
+        context.close();
+    }
+
+    @Test
+    void testLifeCycleServer() {
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
+        context.registerShutdownHook();
+
+        Server server = context.getBean(Server.class);
+
+        Assertions.assertNotNull(server);
 
         context.close();
     }
