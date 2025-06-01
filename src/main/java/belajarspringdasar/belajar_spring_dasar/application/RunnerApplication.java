7d1a50e0817bcae671ad88da16c83179636c4a3f
Karungg <miftahfadilah71@gmail.com>
2025-06-01 14:48:43 +0700

    Final

--- src/main/java/belajarspringdasar/belajar_spring_dasar/application/RunnerApplication.java
+++ src/main/java/belajarspringdasar/belajar_spring_dasar/application/RunnerApplication.java
@@ -1,5 +1,24 @@
 package belajarspringdasar.belajar_spring_dasar.application;
 
+import java.util.List;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
 public class RunnerApplication {
-    
+
+    @Slf4j
+    @Component
+    public static class SimpleRunner implements ApplicationRunner {
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            List<String> profiles = args.getOptionValues("profiles");
+            log.info("Profiles {}", profiles);
+        }
+    }
 }
