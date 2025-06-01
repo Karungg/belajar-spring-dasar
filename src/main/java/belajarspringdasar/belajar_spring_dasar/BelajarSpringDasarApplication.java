11eeddf9960e9efe741ace78efa1ae94ff65d11d
Karungg <miftahfadilah71@gmail.com>
2025-06-01 14:42:55 +0700

    Spring application event

--- src/main/java/belajarspringdasar/belajar_spring_dasar/BelajarSpringDasarApplication.java
+++ src/main/java/belajarspringdasar/belajar_spring_dasar/BelajarSpringDasarApplication.java
@@ -3,7 +3,7 @@ package belajarspringdasar.belajar_spring_dasar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class BelajarSpringDasarApplication {
 
 	public static void main(String[] args) {
--- src/main/java/belajarspringdasar/belajar_spring_dasar/application/FooApplication.java
+++ src/main/java/belajarspringdasar/belajar_spring_dasar/application/FooApplication.java
@@ -1,19 +1,27 @@
 package belajarspringdasar.belajar_spring_dasar.application;
 
+import java.util.List;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import belajarspringdasar.belajar_spring_dasar.data.Foo;
+import belajarspringdasar.belajar_spring_dasar.listener.AppStartingListener;
 
-// @SpringBootApplication
-@Configuration
+@SpringBootApplication
 public class FooApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(FooApplication.class, args);
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.setListeners(List.of(
+                new AppStartingListener()));
+
+        ApplicationContext context = application.run(args);
         Foo foo = context.getBean(Foo.class);
+
     }
 
     @Bean(value = "fooApplicationAnnotation")
